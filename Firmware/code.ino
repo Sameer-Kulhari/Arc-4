b3f9f0fec3a8928ca092062861a4348dd4ad53b3
Sameer <sameerkulhari2439@gmail.com>
2025-07-05 00:23:55 +0530

    Update code.ino

--- Firmware/code.ino
+++ Firmware/code.ino
@@ -1,1 +1,24 @@
+// Define pin numbers
+const int buttonPins[4] = {0, 1, 2, 3};  // pins for buttons
+const int ledPins[4] = {4, 6, 7, 29};     // pins for LEDs
 
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW); 
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 4; i++) {
+    bool isPressed = digitalRead(buttonPins[i]) == LOW;
+
+    digitalWrite(ledPins[i], isPressed ? HIGH : LOW);
+  }
+
+  delay(10); 
+}
